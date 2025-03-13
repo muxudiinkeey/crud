e9f3473fbf2a1e24bfa47cc79e3416496fdaa023
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-13 13:09:14 +0100

    form waa ok

--- users/src/app/add-user/add-user.component.ts
+++ users/src/app/add-user/add-user.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
 
+  addUserForm : FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder){
+    this.addUserForm = this.fb.group({
+     name: [''],
+     username: [''],
+     email: [''],
+    })
+  };
+
+
+  onSubmit(){
+    console.table(this.addUserForm.value);
+  }
+  onCancel(){
+this.router.navigateByUrl('crud');
+  }
 }
